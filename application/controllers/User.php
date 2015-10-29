a82c0f0ac461e1197102e45551e2585c9cb6ab2c
Prasanth <prasanth.ifet@gmail.com>
2015-10-29 13:25:46 +0000

    Codigniter framework :: First Commit

---
+++ application/controllers/User.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	public function login()	{
+		$this->load->view('test');
+	}
+	
+	public function logout() {
+
+	}
+
+	public function create() {
+
+	}
+
+	public function update() {
+		
+	}
+
+}
